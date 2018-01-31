977746cab686bc883583f265ee0403f5449fdc85
Cezar Sa Espinola <cezarsa@gmail.com>
2018-01-31 10:10:56 -0200

    initial commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"sync"
+
+	mgo "github.com/globalsign/mgo"
+	"github.com/tsuru/config"
+	"github.com/tsuru/tsuru/db"
+	"github.com/tsuru/tsuru/db/storage"
+	_ "gopkg.in/mgo.v2"
+)
+
+type obj struct {
+	A string `bson:"_id"`
+	B int
+}
+
+func test(coll *storage.Collection, i, j int) error {
+	defer coll.Close()
+	id := fmt.Sprintf("test-%d", i)
+	_, err := coll.UpsertId(id, obj{A: id, B: 999})
+	if err != nil {
+		return err
+	}
+	var x obj
+	err = coll.FindId(id).One(&x)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			return nil
+		}
+		return err
+	}
+	if x.B != 999 {
+		return errors.New("invalid obj")
+	}
+	err = coll.RemoveId(id)
+	if err == mgo.ErrNotFound {
+		return nil
+	}
+	return err
+}
+
+func main() {
+	runtime.GOMAXPROCS(10)
+	nGoroutines := 200
+	config.Set("database:url", "127.0.0.1:27017")
+	config.Set("database:name", "tsuru_mongodb_stress_test")
+	stor, _ := db.Conn()
+	err := stor.Apps().Database.DropDatabase()
+	if err != nil {
+		panic(err)
+	}
+	stor.Close()
+	wg := sync.WaitGroup{}
+	for g := 0; g < nGoroutines; g++ {
+		wg.Add(1)
+		go func(g int) {
+			defer wg.Done()
+			for i := 0; i < 100; i++ {
+				stor, err := db.Conn()
+				if err != nil {
+					panic(err)
+				}
+				err = test(stor.Apps(), i, g)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}(g)
+	}
+	wg.Wait()
+}
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SED=$(command -v gsed || command -v sed)
+
+function setmgo() {
+    target="$1"
+    find ./vendor/github.com/tsuru -name "*.go" -exec ${SED} -i'' 's|"github.com/globalsign/mgo|"'"$target"'|g' {} +
+    find ./vendor/github.com/tsuru -name "*.go" -exec ${SED} -i'' 's|"gopkg.in/mgo.v2|"'"$target"'|g' {} +
+    ${SED} -i'' 's|mgo "github.com/globalsign/mgo|mgo "'"$target"'|g' main.go
+    ${SED} -i'' 's|mgo "gopkg.in/mgo.v2|mgo "'"$target"'|g' main.go
+}
+
+function runtest() {
+    target="$1"
+    setmgo "$target"
+    failurecount=0
+    tries=5
+    echo "RUNNING: $target - n=$tries"
+    go build -i
+    for i in $(seq $tries); do
+        ./mgostress 2>/dev/null
+        exitcode="$?"
+        if [[ "$exitcode" != "0" ]]; then
+            ((failurecount++))
+        fi
+    done
+    result=$(echo "scale=2; (${failurecount}/${tries})*100" | bc)
+    echo "TARGET $target - failures: ${result}%"
+}
+
+runtest "gopkg.in/mgo.v2"
+runtest "github.com/globalsign/mgo"
